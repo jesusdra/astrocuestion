4db707337bd84e7ac142d25f19bbb7a63e4d7c48
Jesus <32868929+jesusdra@users.noreply.github.com>
2025-03-21 13:31:45 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://artulance.github.io',
+    site: 'https://jesusdra.github.io',
     base: 'astrocuestion',
 });
